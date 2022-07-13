dfca4f43d20ba75a11f1299f2fb4d447a80bb429
Alex Bene <theredhunter00@gmail.com>
2022-07-13 19:59:02 +0300

    modified:   README.md 	modified:   src/main.rs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # [jubilant-octo-fiesta](https://c.tenor.com/s4zwGBwaNC0AAAAd/johnny-trolling.gif)
 
-### mostest epicest github repo ever!!!!!1
+~cargo run :D
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::process::Command;
+
 fn main() 
 {
-    println!("Hello, world!");
+    Command::new("zsh")
+            .args(["-c","firefox www.youtube.com/watch\\?v=dQw4w9WgXcQ"])
+            .output()
+            .expect("failed to execute process");
 }
